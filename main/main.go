d6b7cde9ca0ff6855f370bd068767a03e314818f
kekelberi <alexei.gaponov@gmail.com>
2022-07-07 17:50:47 +0300

    save

--- main/main.go
+++ main/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
 	fmt.Println("hi")
+
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	db.SetConnMaxLifetime(20 * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
 }
