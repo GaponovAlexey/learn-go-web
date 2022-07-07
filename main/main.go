dbde4e660dde99fb5799e7af74871ef4c2dce755
kekelberi <alexei.gaponov@gmail.com>
2022-07-07 20:05:14 +0300

    save

--- main/main.go
+++ main/main.go
@@ -15,6 +15,15 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html")
+	if err != nil {
+		fmt.Println(w, err.Error())
+		return
+	}
+	t.ExecuteTemplate(w, "create", nil)
+}
+
 func main() {
 	HandleFunc()
 }
@@ -22,5 +31,6 @@ func main() {
 func HandleFunc() {
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates/"))))
 	http.HandleFunc("/", mainPage)
+	http.HandleFunc("/create", create)
 	http.ListenAndServe(":3000", nil)
 }
